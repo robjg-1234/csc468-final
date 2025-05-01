8f88ee39ecaeceb0e57dccc0c9ee7e5cf46cadbb
Roberto Guerra <rg8156a@student.american.edu>
2025-05-01 17:28:02 -0400

    Added a explanation of each algorithm

--- aisearch/Assets/Scripts/AISearch.cs
+++ aisearch/Assets/Scripts/AISearch.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class AISearch
 {
-
+    //Grabs all the neighbors directly to the sides of the starting node which means that it doesn't move diagonally 
     private static (int, int)[] GetNeighbors(int posX, int posY, bool[,] map)
     {
         List<(int, int)> neighbors = new List<(int, int)>();
@@ -49,6 +49,7 @@ public class AISearch
 
 
     //non-recursive dfs
+    // Uses a stack to go to the node that was added at last, due to how it is added the algorithm has a bias of going up first.
     public static (List<(int, int)>, (int, int)[]) NRDepthFirstSearch((int, int) startingPoint, (int, int) goalPoint, bool[,] map)
     {
         int nodesVisits = 1;
@@ -80,6 +81,7 @@ public class AISearch
                 seen.Add(currentNode);
                 foreach ((int, int) i in GetNeighbors(currentNode.Item1, currentNode.Item2, map))
                 {
+                    // A node gets added to the stack only if it is not in the stack and it hasn't been visited.
                     if (!nodeStack.Contains(i) && !seen.Contains(i))
                     {
                         relations.Add((currentNode, i));
@@ -90,6 +92,7 @@ public class AISearch
         }
         if (found)
         {
+            // Backtracks to show the final path.
             List<(int, int)> curatedPath = new List<(int, int)>() { goalPoint };
             (int, int) currentNode = curatedPath[0];
             while (!curatedPath.Contains(startingPoint))
@@ -105,15 +108,18 @@ public class AISearch
             }
             curatedPath.Reverse();
             path = curatedPath.ToArray();
+            //It returns the nodes visited and the path
             return (seen, path);
         }
         else
         {
+            //If the path was not found it returns all the nodes visited and an array with a value of (-1,-1) to indicate that it didn't found a valid path.
             return (seen, new (int, int)[] { (-1, -1) });
         }
     }
 
     // BFS
+    //The breadth-first search algorithm uses a queue to keep track of the nodes in a first in first out basis.
     public static (List<(int, int)>, (int, int)[]) BreadthFirstSearch(int startingX, int startingY, int goalX, int goalY, bool[,] map)
     {
         int nodesVisited = 0;
@@ -137,6 +143,7 @@ public class AISearch
             nodesVisited++;
             if (currentnode.Item1 == goalX && currentnode.Item2 == goalY)
             {
+                // if the goal is found the queue gets cleared
                 seen.Add(currentnode);
                 nodeQueue.Clear();
                 found = true;
@@ -147,6 +154,7 @@ public class AISearch
                 seen.Add(currentnode);
                 for (int i = 0; i < newNodes.Length; i++)
                 {
+                    // A node is added to the queue if it is not in the queue and it hasn't been seen.
                     if (!seen.Contains(newNodes[i]) && !nodeQueue.Contains(newNodes[i]))
                     {
                         nodeQueue.Enqueue(newNodes[i]);
@@ -155,6 +163,7 @@ public class AISearch
                 }
             }
         }
+        //Backtracks to see if there is a valid path, if there isn'y it returns a path with the (-1.-1) value in the other case it returns the valid path. It also returns the nodes explored.
         if (found)
         {
             List<(int, int)> curatedPath = new List<(int, int)>() { (goalX, goalY) };
@@ -180,8 +189,10 @@ public class AISearch
         }
     }
     //AStar Implementation
+    // This algorithm uses a dictionary as a priority queue to pick the one with the lowest fscore.
     static (int, int)[] BacktrackPath((int, int)[,] relationMap, (int, int) currentSpot)
     {
+        // This backtracks using a relationship map and is used for both the greedy search and the Astar algorithm.
         List<(int, int)> path = new List<(int, int)>();
         (int, int) currentNode = currentSpot;
         while (currentNode != (-1, -1))
@@ -195,6 +206,7 @@ public class AISearch
 
     static float HeuristicFormula((int, int) currentNode, (int, int) endPoint)
     {
+        // the hueristic formula used is the Manhattan distance.
         return Mathf.Abs(currentNode.Item1 - endPoint.Item1) + Mathf.Abs(currentNode.Item2 - endPoint.Item2);
     }
 
@@ -211,6 +223,7 @@ public class AISearch
         float[,] fScore = new float[mapWidth, mapHeight];
         (int, int)[,] relationMap = new (int, int)[mapWidth, mapHeight];
         relationMap[startingPoint.Item1, startingPoint.Item2] = (-1, -1);
+        // Initialize the fscore and gscore map with the maximum possible value
         for (int i = 0; i < mapWidth; i++)
         {
             for (int j = 0; j < mapHeight; j++)
@@ -219,6 +232,7 @@ public class AISearch
                 fScore[i, j] = float.MaxValue;
             }
         }
+        // Set the starting point gScore and fScore value to the starting values.
         gScore[startingPoint.Item1, startingPoint.Item2] = 0f;
         fScore[startingPoint.Item1, startingPoint.Item2] = HeuristicFormula(startingPoint, goalPoint);
 
@@ -227,6 +241,7 @@ public class AISearch
         {
             nodesVisits++;
             (int, int) currentNode = (-1, -1);
+            // This finds which node has the lowest fScore of the available nodes
             foreach (KeyValuePair<(int, int), float> keys in nodeQueue)
             {
                 if (currentNode == (-1, -1))
@@ -239,6 +254,7 @@ public class AISearch
                     {
                         currentNode = keys.Key;
                     }
+                    // This helps the algorithm speed up by also choosing the one that is physically closest to the endpoint in case there is a tie.
                     else if ((keys.Value == nodeQueue[currentNode]) && (HeuristicFormula(currentNode, goalPoint) - HeuristicFormula(keys.Key, goalPoint) > 0) && (HeuristicFormula(currentNode, goalPoint) - HeuristicFormula(keys.Key, goalPoint) < 3))
                     {
                         currentNode = keys.Key;
@@ -263,7 +279,8 @@ public class AISearch
                 {
                     if (i != (-1, -1))
                     {
-
+                        //Establishes the gScore and fScore if it is lower than its existing value.
+                        // The cost for moving is always 1 + the gScore of the current node that is visiting it.
                         float tentativeGScore = gScore[currentNode.Item1, currentNode.Item2] + 1;
                         if (tentativeGScore < gScore[i.Item1, i.Item2])
                         {
@@ -283,10 +300,12 @@ public class AISearch
                             nodesSeen.Add(i);
                         }
                     }
+                    //This helps guarantee that there are no blank spots in the exploration process
                     foreach ((int, int) j in GetNeighbors(i.Item1, i.Item2, map))
                     {
                         if (j != (-1, -1))
                         {
+                            // Establishes the gScore of the nieghbors in case we visit it from a different position later in the exploration process
                             float tentativeGScore = gScore[i.Item1, i.Item2] + 1;
                             if (tentativeGScore < gScore[j.Item1, j.Item2])
                             {
@@ -303,12 +322,14 @@ public class AISearch
                 }
             }
         }
+        // returns all the nodes that had there gScore modified and the actual path to the end goal or (-1,-1) if it didn't find anything.
         return (nodesSeen, foundPath);
     }
 
     //Greedy Search
     public static (List<(int, int)>, (int, int)[]) GreedySearch((int, int) startingPoint, (int, int) goalPoint, bool[,] map)
     {
+        // This utilizes a similar approach to the A* when it comes to looking for the lowest cost but the cost is only calculated at the immediate position which means that it only considers the calculated distance to the end point
         int nodesVisits = 0;
         int mapWidth = map.GetLength(0);
         int mapHeight = map.GetLength(1);
@@ -330,6 +351,7 @@ public class AISearch
         {
             nodesVisits++;
             (int, int) currentNode = (-1, -1);
+            // Chooses the node that is the closest to the endpoint from the available nodes
             for (int i = 0; i < nodeQueue.Count; i++)
             {
                 if (currentNode != (-1, -1))
@@ -346,6 +368,7 @@ public class AISearch
             }
             if (currentNode == goalPoint)
             {
+                // If it reaches the end point it returns all the nodes visited and the path
                 nodesVisited.Add(currentNode);
                 foundPath = BacktrackPath(relationMap, currentNode);
                 return (nodesVisited, foundPath);
@@ -368,11 +391,12 @@ public class AISearch
             }
 
         }
-
+        // If it doesnt find the end point it returns an array with (-1,-1) alongside all the nodes visited
         return (nodesVisited, foundPath);
     }
 
     //Random Exploration Search
+    // This algorithm randomly chooses which direction to go as long as it hasn't already been visited
     public static (List<(int, int)>, (int, int)[]) RandomSearch((int, int) startingPoint, (int, int) goalPoint, bool[,] map)
     {
         int nodesVisits = 0;
